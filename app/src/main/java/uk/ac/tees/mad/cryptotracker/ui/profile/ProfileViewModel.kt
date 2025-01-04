da3ac4618f762d593ca449a4915518b58f3f4cd6
SaiRam420117 <teessidesairam@gmail.com>
2025-01-05 04:31:20 +0530

    Commit_15

--- app/src/main/java/uk/ac/tees/mad/cryptotracker/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/uk/ac/tees/mad/cryptotracker/ui/profile/ProfileViewModel.kt
@@ -4,13 +4,16 @@ import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import uk.ac.tees.mad.cryptotracker.models.ProfileState
+import uk.ac.tees.mad.cryptotracker.models.UserProfile
 
 class ProfileViewModel : ViewModel() {
     private val auth = FirebaseAuth.getInstance()
     private val firestore = FirebaseFirestore.getInstance()
 
-    private val _userProfile = MutableStateFlow(UserProfile())
-    val userProfile = _userProfile.asS
+    private val _userProfile = MutableStateFlow<ProfileState>(ProfileState.Initial)
+    val userProfile = _userProfile.asStateFlow()
 
     init {
         fetchUserProfile()
@@ -18,28 +21,29 @@ class ProfileViewModel : ViewModel() {
 
     fun fetchUserProfile() {
         val userId = auth.currentUser?.uid ?: return
-
+        _userProfile.value = ProfileState.Loading
         firestore.collection("users").document(userId).get()
             .addOnSuccessListener { documentSnapshot ->
                 documentSnapshot.toObject(UserProfile::class.java)?.let {
-                    _userProfile.value = it
+                    _userProfile.value = ProfileState.Success(it)
                 }
             }
             .addOnFailureListener {
-                // Handle failure
+                it.printStackTrace()
             }
     }
 
     fun updateUserProfile(username: String, email: String) {
         val userId = auth.currentUser?.uid ?: return
-        val updatedProfile = hashMapOf("username" to username, "email" to email)
-
+        val updatedProfile = mapOf("username" to username, "email" to email)
+        _userProfile.value = ProfileState.Loading
         firestore.collection("users").document(userId).update(updatedProfile)
             .addOnSuccessListener {
-                _userProfile.value = UserProfile(username, email)
+                _userProfile.value =
+                    ProfileState.Success(UserProfile(username = username, email = email))
             }
             .addOnFailureListener {
-                // Handle failure
+                it.printStackTrace()
             }
     }
 }
