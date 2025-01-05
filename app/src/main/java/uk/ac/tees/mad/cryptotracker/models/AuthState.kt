3353e83e843bcf49363aec71ce61f1737cc339d5
SaiRam420117 <teessidesairam@gmail.com>
2025-01-05 22:51:15 +0530

    Commit_17

--- app/src/main/java/uk/ac/tees/mad/cryptotracker/models/AuthState.kt
+++
@@ -1,8 +0,0 @@
-package uk.ac.tees.mad.cryptotracker.models
-
-sealed class AuthState {
-    data object Initial : AuthState()
-    data object Loading : AuthState()
-    data object Success : AuthState()
-    data class Error(val message: String) : AuthState()
-}
--- app/src/main/java/uk/ac/tees/mad/cryptotracker/models/ProfileState.kt
+++
@@ -1,8 +0,0 @@
-package uk.ac.tees.mad.cryptotracker.models
-
-sealed class ProfileState {
-    data object Initial : ProfileState()
-    data object Loading : ProfileState()
-    data class Success(val userProfile: UserProfile) : ProfileState()
-    data class Error(val message: String) : ProfileState()
-}
