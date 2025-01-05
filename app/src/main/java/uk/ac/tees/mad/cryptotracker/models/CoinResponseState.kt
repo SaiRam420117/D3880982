d8a4ca91616bf65d0f97f247055b42af4e8a3916
SaiRam420117 <teessidesairam@gmail.com>
2025-01-05 22:58:42 +0530

    Commit_20

--- app/src/main/java/uk/ac/tees/mad/cryptotracker/models/CoinResponseState.kt
+++
@@ -1,8 +0,0 @@
-package uk.ac.tees.mad.cryptotracker.models
-
-sealed class CoinResponseState {
-    data object Initial : CoinResponseState()
-    data object Loading : CoinResponseState()
-    data class Success(val coins: CoinDetails) : CoinResponseState()
-    data class Error(val message: String) : CoinResponseState()
-}
--- app/src/main/java/uk/ac/tees/mad/cryptotracker/models/UserProfile.kt
+++
@@ -1,6 +0,0 @@
-package uk.ac.tees.mad.cryptotracker.models
-
-data class UserProfile(
-    val username: String = "",
-    val email: String = ""
-)
